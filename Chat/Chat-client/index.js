cac3a5668bf344d4e2aad8f957d633ec5995245f
daresh887 <daresh.hanganu@gmail.com>
2021-10-23 16:44:25 +0300

    Added a chat network.

---
+++ Chat/Chat-client/index.js
@@ -0,0 +1,23 @@
+const io = require('socket.io-client');
+const socket = io("http://192.168.1.179:3000/");
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+});
+console.log("What is your name?");
+rl.question("What is your name?", (text) => {
+    socket.emit('new user', text.trim());
+    console.log("You joined the chat");
+    process.stdout.write("> ");
+});
+socket.on("message", (text) => {
+    process.stdout.write("\r\x1b[K")
+    console.log(text);
+    process.stdout.write("> ");
+});
+rl.prompt();
+rl.on('line', (text) => {
+    socket.emit('message', text.trim());
+    process.stdout.write("> ");
+    rl.prompt();
+});
