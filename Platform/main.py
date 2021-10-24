1fa27a1ab795fe787c1a90178b33d4f4dfeaa705
robol88 <rares.hanganur@gmail.com>
2021-10-24 16:37:40 +0300

    Update main.py

--- Platform/main.py
+++ Platform/main.py
@@ -4,7 +4,7 @@ from tkinter import font
 root = Tk()
 root.geometry("300x250")
 root.title("Blue Tiger")
-root.iconbitmap('images/tigru_albastru.ico')
+root.iconbitmap('c:/users/rares/documents/github/bluetiger/platform/images/tigru_albastru.ico')
 
 login_text = Label(
     root,
